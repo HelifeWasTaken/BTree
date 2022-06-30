877c6a009e9e0a5daa0c7ce9f354df65cec9b281
mattis - Helife <mattis.dalleau@epitech.eu>
2022-06-30 11:55:01 +0400

    Made Btree compile with proper C++

--- BTree.hpp
+++ BTree.hpp
@@ -16,15 +16,17 @@
 * along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-static_assert(
-    (
-        (defined(_MSVC_LANG) && _MSVC_LANG >= 201703L) ||
-        __cplusplus >= 201703L
-    ),
-    "BtreeImplementation requires C++17 or higher (std::optional)"
-);
+#pragma once
+
+#ifdef _MSVC_LANG
+static_assert(defined(_MSVC_LANG) && _MSVC_LANG >= 201703L && "BtreeImplementation requires C++17 or higher (std::optional)");
+#else
+static_assert(__cplusplus >= 201703L, "BtreeImplementation requires C++17 or higher (std::optional)");
+#endif
 
 #include <optional>
+#include <memory>
+#include <functional>
 
 #ifndef BTREE_NAMESPACE
 #define BTREE_NAMESPACE btree
@@ -33,15 +35,25 @@ static_assert(
 namespace BTREE_NAMESPACE {
 
 template<typename T>
-class BtreeNode {
-public:
-    using BtreeValue = std::optional<BtreeNode<T>>;
-    using BtreeNoValue = std::nullopt;
-    using BtreeCmp = std::function<bool(const T& toInsert, const T& cNode)>;
+class BtreeNode;
 
+template<typename T>
+using BtreeValue = std::unique_ptr<BtreeNode<T>>;
+
+template<typename T>
+using BtreeCmp = std::function<bool(const T& toInsert, const T& cNode)>;
+
+template<typename T>
+using BtreeSearchResult = std::optional<std::reference_wrapper<T>>;
+
+template<typename T>
+using BtreeSearchResultConst = const std::optional<const std::reference_wrapper<const T>>;
+
+template<typename T>
+class BtreeNode {
 private:
-    BtreeValue<T> _left = BtreeNoValue;
-    BtreeValue<T> _right = BtreeNoValue;
+    BtreeValue<T>  _left = nullptr;
+    BtreeValue<T> _right = nullptr;
     T _value;
     bool _visited = false;
 
@@ -52,8 +64,13 @@ public:
 
     // The value passed as parameter in reference will be moved
     BtreeNode(T &value)
-        : _value(std::move(value))
+        : _value(value)
     {}
+
+    static BtreeValue<T> makeNode(T &value)
+    { return std::make_unique<BtreeNode<T>>(value); }
+    static BtreeValue<T> makeNode(T &&value)
+    { return std::make_unique<BtreeNode<T>>(std::move(value)); }
 
     // Returns the value in the object
     T &get() { return _value; }
@@ -61,62 +78,81 @@ public:
     const T &get() const { return _value; }
 
     // Returns the left element of the BinaryTree
-    BtreeValue& left() { return _left; }
+    BtreeValue<T>& left() { return _left; }
     // Returns the left element of the BinaryTree (const)
-    const BtreeValue& left() const { return _left; }
+    const BtreeValue<T>& left() const { return _left; }
 
     // Returns the Right element of the BinaryTree
-    BtreeValue& right() { return _right; }
+    BtreeValue<T>& right() { return _right; }
     // Returns the Right element of the BinaryTree (const)
-    const BtreeValue& right() const { return _right; }
+    const BtreeValue<T>& right() const { return _right; }
 
     // Takes the value as parameter
     // If cmp returns false Left is choosen otherwise right is choosen
-    void insert(T &value, const BtreeCmp& cmp) {
-        if (cmp(value, _value)) {
-            if (_left) _left.insert(value, cmp);
-            else _left = { BtreeNode<T>(value) };
+    void insert(T &value, const BtreeCmp<T>& cmp, const bool& allow_duplicates) {
+        if (!allow_duplicates && (value == _value)) {
+            return;
+        } else if (cmp(value, _value)) {
+            if (_left.get()) _left->insert(value, cmp, allow_duplicates);
+            else _left = makeNode(value);
         } else {
-            if (_right) _right.insert(value, cmp);
-            else _right = { BtreeNode<T>(value) };
+            if (_right.get()) _right->insert(value, cmp, allow_duplicates);
+            else _right = makeNode(value);
+        }
+    }
+
+    // Takes the value as parameter
+    // If cmp returns false Left is choosen otherwise right is choosen
+    // Is used to move the value
+    void insert(T &&value, const BtreeCmp<T>& cmp, const bool& allow_duplicates) {
+        if (!allow_duplicates && (value == _value)) {
+            return;
+        } else if (cmp(value, _value)) {
+            if (_left.get()) _left->insert(value, cmp, allow_duplicates);
+            else _left = makeNode(value);
+        } else {
+            if (_right.get()) _right->insert(value, cmp, allow_duplicates);
+            else _right = makeNode(value);
         }
     }
 
     // Takes a value to research as parameter and the comparator
     // If cmp returns false Left is choosen otherwise right is choosen
-    std::optional<T&> search(const T& value, const BtreeCmp& cmp) {
-        if (_value == value) return { _value };
-        if (cmp(value, _value)) {
-            if (_left) return _left.search(value, cmp);
+    BtreeSearchResult<T> search(const T& value, const BtreeCmp<T>& cmp) {
+        if (_value == value) {
+            return { _value };
+        } else if (cmp(value, _value)) {
+            if (_left.get()) return _left->search(value, cmp);
             else return std::nullopt;
         } else {
-            if (_right) return _right.search(value, cmp);
+            if (_right.get()) return _right->search(value, cmp);
             else return std::nullopt;
         }
     }
 
     // See search method (Same as const)
-    const std::optional<const T&> search(const T& value, const BtreeCmp& cmp) const {
-        if (_value == value) return { _value };
-        if (cmp(value, _value)) {
-            if (_left) return _left.search(value, cmp);
+    BtreeSearchResultConst<T> search(const T& value, const BtreeCmp<T>& cmp) const {
+        if (_value == value) {
+            return { _value };
+        } else if (cmp(value, _value)) {
+            if (_left.get()) return _left->search(value, cmp);
             else return std::nullopt;
         } else {
-            if (_right) return _right.search(value, cmp);
+            if (_right.get()) return _right->search(value, cmp);
             else return std::nullopt;
         }
     }
 
     // Tells wheter this node has been visited
-    const bool& isVisited const { return _visited; }
+    const bool& isVisited() const { return _visited; }
 
     // Triggers visited to false
     // Does not set unvisited on all subNodes by default
     void unvisit(const bool& unvisitSubNodes=false) {
         _visited = false;
         if (unvisitSubNodes) {
-            if (_left) _left.unvisit(unvisitSubNodes);
-            if (_right) _right.unvisit(unvisitSubNodes);
+            if (_left.get()) _left->unvisit(unvisitSubNodes);
+            if (_right.get()) _right->unvisit(unvisitSubNodes);
         }
     }
 
@@ -127,41 +163,41 @@ public:
 template<typename T>
 class Btree {
 private:
-    BtreeNode<T>::BtreeValue _root;
-    BtreeNode<T>::BtreeCmp _cmp;
+    BtreeValue<T> _root = nullptr;
+    BtreeCmp<T> _cmp;
+    bool _allow_duplicates;
 
 public:
-    Btree(const BtreeNode<T>::BtreeCmp& cmp)
+    Btree(const BtreeCmp<T>& cmp, const bool &allow_duplicates=true)
         : _cmp(cmp)
-    {}
-
-    Btree(const BtreeNode<T>::BtreeCmp& cmp, T& value)
-        : _root(value)
-        , _cmp(cmp)
-    {}
-
-    Btree(const BtreeNode<T>::BtreeCmp& cmp, T&& value)
-        : _root(value)
-        , _cmp(cmp)
+        , _allow_duplicates(allow_duplicates)
     {}
 
     Btree& insert(T& value) {
-        if (_root) _root = { BtreeNode(value) };
-        else _root.insert(value, cmp);
+        if (!_root.get()) _root = BtreeNode<T>::makeNode(value);
+        else _root->insert(value, _cmp, _allow_duplicates);
         return *this;
     }
 
-    std::optional<T&> search(const T& value) {
-        return _root ? _root.search(value, _cmp) : std::nullopt;
+    Btree& insert(T&& value) {
+        if (!_root.get()) _root = BtreeNode<T>::makeNode(std::move(value));
+        else _root->insert(value, _cmp, _allow_duplicates);
+        return *this;
     }
 
-    const std::optional<const T&> search(const T& value) const {
-        return _root ? _root.search(value, _cmp) : std::nullopt;
+    BtreeSearchResult<T> search(const T& value) {
+        return _root.get() ? _root->search(value, _cmp) : std::nullopt;
     }
 
-    BtreeNode<T>::BtreeValue& root() { return _root; }
-    const BtreeNode<T>::BtreeValue& root() const { return _root; }
+    BtreeSearchResultConst<T> search(const T& value) const {
+        return _root.get() ? _root->search(value, _cmp) : std::nullopt;
+    }
 
-    Btree& unvisitNodes() { _root.unvisit(true); }
+    BtreeValue<T>& root() { return _root; }
+    const BtreeValue<T>& root() const { return _root; }
+
+    Btree& unvisitNodes() {
+        _root->unvisit(true); return *this;
+    }
 };
 }
